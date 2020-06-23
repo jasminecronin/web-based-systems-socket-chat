2b7f922d875e9c5ab31dcb91cf1196a2d7d51d67
Jasmine Roebuck <roebuckj3@gmail.com>
2020-06-23 17:05:17 -0600

    Removed print debugging statements

--- index.js
+++ index.js
@@ -12,14 +12,12 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    //users[socket.id]
     var username = "User" + Math.floor(Math.random() * 1000);
     users[socket.id] = {
         name: username,
         color: "#c6ccd8"
     };
 
-    // socket.emit('joined', users[socket.id]);
     socket.emit('joined', users);
     socket.emit('populate', history);
     updateUsers();
@@ -35,23 +33,16 @@ io.on('connection', function(socket){
         history.push(chatMessage);
         if (history.length > 200) history.shift();
 
-        // io.emit('chat message', momentTimestamp.bold() + "\t\t" + msg);
         io.emit('chat message', {
             name: users[socket.id].name,
             color: users[socket.id].color,
             text: msg,
             timestamp: momentTimestamp
         });
-        // console.log('message: ', msg);
     });
 
     socket.on('updateUsername', function(name) {
         users[socket.id].name = name;
-        // for (var id in users) {
-        //     if (id !== socket.id && users[id].name === name) {
-        //         delete users[id];
-        //     }
-        // }
         updateUsers();
     });
 
@@ -60,7 +51,6 @@ io.on('connection', function(socket){
     });
 
     socket.on('disconnect', function() {
-        // console.log('test', users[socket.id]);
         delete users[socket.id];
         updateUsers();
     });
